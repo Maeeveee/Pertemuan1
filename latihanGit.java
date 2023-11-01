10e6693db853ad16b20fc11dd734e550d54f91a4
Maeeveee <142636445+Maeeveee@users.noreply.github.com>
2023-11-01 20:05:03 +0700

    mbah scaner

--- latihanGit.java
+++ latihanGit.java
@@ -1,8 +1,13 @@
 package latihanGit;
+    import java.util.Scanner;
     public class latihanGit {
+        
     public static void main(String[] args) {
+
+        Scanner sc2 = new Scanner(System.i)
         System.out.println("hello world");
         System.out.println("hai");
+        
 
     }
         
