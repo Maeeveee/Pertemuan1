2c2df0ef6078f472c6bf2a6d0d5c731c70a8bd24
Maeeveee <142636445+Maeeveee@users.noreply.github.com>
2023-11-01 19:57:12 +0700

    menambah hai

--- latihanGit.java
+++ latihanGit.java
@@ -2,6 +2,8 @@ package latihanGit;
     public class latihanGit {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hai");
+
     }
         
     }
